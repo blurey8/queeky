cec4f75e64a4ab18624b20f5768d2af5eea8dde5
blurey8 <reyhanhamidi@gmail.com>
2018-11-04 00:42:46 +0700

    ternyata cuman perkara /callback di URL ngrok

---
+++ index.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, abort
+from dotenv import load_dotenv, find_dotenv
+import os
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+load_dotenv(find_dotenv())
+
+line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info('Request body: {}'.format(body)) # Debug
+    # app.logger.info(os.environ.get('CHANNEL_ACCESS_TOKEN'))
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if (event.message.text == '.profile'):
+        profile = line_bot_api.get_profile(event.source.user_id)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(
+                text='Name: {}\nUser ID: {}\nPicture URL: {}\
+                \nStatus: {}'.format(
+                    profile.display_name,
+                    profile.user_id,
+                    profile.picture_url,
+                    profile.status_message
+                )
+            )
+        )
+    else:
+        # Echo
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
+
+if __name__ == "__main__":
+    app.run()
